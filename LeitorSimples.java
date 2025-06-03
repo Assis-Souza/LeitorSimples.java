5e6198da48cb58b1d165e09edfbd2081945d0f24
Assis-Souza <guh50091@gmail.com>
2025-06-03 20:50:41 -0300

    Add files via upload

--- LeitorSimples.java
+++ LeitorSimples.java
@@ -1,3 +1,20 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 public class LeitorSimples {
-    
+ @SuppressWarnings("ConvertToTryWithResources")
+ public static void main(String[] args) {
+ try {
+     try (FileReader leitor = new FileReader("frase.txt")) {
+         BufferedReader buffer = new BufferedReader(leitor);
+         String linha;
+         while ((linha = buffer.readLine()) != null) {
+             System.out.println("Frase lida: " + linha);
+         }
+         buffer.close();
+     }
+ } catch (IOException e) {
+ System.out.println("Erro ao ler o arquivo.");
+ }
+ }
 }
